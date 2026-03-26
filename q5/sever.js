63ee4f5ed39fb6b8b2ec32689434a688a80b4cfa
Dimitri Chen <200327july@gmail.com>
2026-03-26 12:12:07 -0400

    Update sever.js

--- q5/sever.js
+++ q5/sever.js
@@ -43,17 +43,16 @@ app.post("/login", (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
-    const query =
-        "SELECT * FROM users WHERE username = '" +
-        username +
-        "' AND password = '" +
-        password +
-        "'"
+    if (typeof username !== "string" || typeof password !== "string") {
+        return res.status(400).send("Invalid input");
+    }
+
+    const query = "SELECT * FROM users WHERE username = ? AND password = ?";
 
     console.log("\nExecuting SQL:")
     console.log(query)
 
-    db.all(query, (err, rows) => {
+    db.all(query, [username, password], (err, rows) => {
 
         if (err) {
             return res.status(500).send("Database error")
